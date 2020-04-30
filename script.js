38f868389ee11890992d93c418bfefa52d123f8b
imedakrouti <imed.akrouti@gmail.com>
2020-04-30 14:13:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.nav-button').click(function(){
+        $('.nav-button').toggleClass("change ")
+    }); 
+    $(window).scroll(function(){
+        let position=$(this).scrollTop();
+        // console.log(position);
+        if(position>=200){
+            $('.nav-menu').addClass('custom-navbar');
+        }else{
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+    });
+    
+});
+
