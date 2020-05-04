6655daa77e384cb3071f037cebff633a59545f80
imedakrouti <imed.akrouti@gmail.com>
2020-05-04 04:05:45 +0100

    final commit

--- script.js
+++ script.js
@@ -11,6 +11,34 @@ $(function () {
             $('.nav-menu').removeClass('custom-navbar');
         }
     });
-    
+    $(window).scroll(function(){
+        let position=$(this).scrollTop();
+        // console.log(position);
+        if(position>=570){
+            $('.camera-img').addClass('from-left');
+            $('.mission-text').addClass('from-right');
+        }else{
+            $('.camera-img').removeClass('from-left');
+            $('.mission-text').removeClass('from-right');
+        }
+    });
+$('.gallery-list-item').click(function(){
+    let value=$(this).attr('data-filter');
+    if(value=="all"){
+        $('.filter').show('3000');
+    }else{
+        $('.filter').not(`.${value}`).hide(3000);
+        $('.filter').filter(`.${value}`).show(3000);
+    }
+});  
+$('.gallery-list-item').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+
+});
+
+$('.nav-item a').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+});
+
 });
 
